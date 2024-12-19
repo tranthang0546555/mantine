20356fc33926533f900e126cb71ec9666b678a99
Wyn <65168044+tranthang0546555@users.noreply.github.com>
2024-12-19 09:14:00 +0000

    recommit

--- mantine-vite/src/pages/analysis-report/index.tsx
+++ mantine-vite/src/pages/analysis-report/index.tsx
@@ -1,10 +1,11 @@
 import { useMemo } from "react";
 import {
   MantineReactTable,
+  MRT_GlobalFilterTextInput,
   useMantineReactTable,
   type MRT_ColumnDef,
 } from "mantine-react-table";
-import { Paper } from "@mantine/core";
+import { Flex, Paper } from "@mantine/core";
 
 type Person = {
   name: {
@@ -18,6 +19,142 @@ type Person = {
 
 //nested data is ok, see accessorKeys in ColumnDef below
 const data: Person[] = [
+  {
+    name: {
+      firstName: "Zachary",
+      lastName: "Davis",
+    },
+    address: "261 Battle Ford",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Robert",
+      lastName: "Smith",
+    },
+    address: "566 Brakus Inlet",
+    city: "Westerville",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Yan",
+    },
+    address: "7777 Kuhic Knoll",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "John",
+      lastName: "Upton",
+    },
+    address: "722 Emie Stream",
+    city: "Huntington",
+    state: "Washington",
+  },
+  {
+    name: {
+      firstName: "Nathan",
+      lastName: "Harris",
+    },
+    address: "1 Kuhic Knoll",
+    city: "Ohiowa",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Zachary",
+      lastName: "Davis",
+    },
+    address: "261 Battle Ford",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Robert",
+      lastName: "Smith",
+    },
+    address: "566 Brakus Inlet",
+    city: "Westerville",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Yan",
+    },
+    address: "7777 Kuhic Knoll",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "John",
+      lastName: "Upton",
+    },
+    address: "722 Emie Stream",
+    city: "Huntington",
+    state: "Washington",
+  },
+  {
+    name: {
+      firstName: "Nathan",
+      lastName: "Harris",
+    },
+    address: "1 Kuhic Knoll",
+    city: "Ohiowa",
+    state: "Nebraska",
+  },
+
+  {
+    name: {
+      firstName: "Zachary",
+      lastName: "Davis",
+    },
+    address: "261 Battle Ford",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Robert",
+      lastName: "Smith",
+    },
+    address: "566 Brakus Inlet",
+    city: "Westerville",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Yan",
+    },
+    address: "7777 Kuhic Knoll",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "John",
+      lastName: "Upton",
+    },
+    address: "722 Emie Stream",
+    city: "Huntington",
+    state: "Washington",
+  },
+  {
+    name: {
+      firstName: "Nathan",
+      lastName: "Harris",
+    },
+    address: "1 Kuhic Knoll",
+    city: "Ohiowa",
+    state: "Nebraska",
+  },
   {
     name: {
       firstName: "Zachary",
@@ -96,6 +233,26 @@ const Example = () => {
   const table = useMantineReactTable({
     columns,
     data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    positionGlobalFilter: "left",
+    state: {
+      showGlobalFilter: true,
+      showColumnFilters: false,
+      // isLoading: true,
+    },
+    mantineSearchTextInputProps: {
+      placeholder: "Mã",
+    },
+    enableStickyHeader: true,
+    paginationDisplayMode: "pages",
+    enableSorting: false,
+    enableGlobalFilter: true,
+    // enableFilters: false,
+    enableDensityToggle: false,
+    enableFullScreenToggle: false,
+    enableColumnActions: false,
+    onGlobalFilterChange: (value) => {
+      // console.log(value);
+    },
   });
 
   return (
